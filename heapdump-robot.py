4171885da8c058e012820778d704810c254ddbee
ret0x7c00 <ret0x7c00@pm.me>
2021-12-27 13:52:03 +0800

    :zap: Improve heapdump bot

--- heapdump-robot.py
+++ heapdump-robot.py
@@ -16,6 +16,56 @@ SLEEP_TIME_MAX = os.environ.get('HEAPDUMP_SLEEP_TIME_MAX') or 6
 session = requests.Session()
 
 
+def visitHome():
+    url = "https://www.heapdump.cn/"
+
+    payload = {}
+    headers = {
+        'authority': 'www.heapdump.cn',
+        'pragma': 'no-cache',
+        'cache-control': 'no-cache',
+        'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="96", "Google Chrome";v="96"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"macOS"',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-user': '?1',
+        'sec-fetch-dest': 'document',
+        'referer': 'https://www.heapdump.cn/',
+        'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8',
+    }
+    response = requests.request("GET", url, headers=headers, data=payload)
+    print(response.status_code)
+
+
+def isSignin():
+    url = "https://www.heapdump.cn/api/community/signin/isSignin"
+
+    payload = {}
+    headers = {
+        'authority': 'www.heapdump.cn',
+        'pragma': 'no-cache',
+        'cache-control': 'no-cache',
+        'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="96", "Google Chrome";v="96"',
+        'accept': 'application/json, text/plain, */*',
+        'dnt': '1',
+        'sec-ch-ua-mobile': '?0',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',
+        'sec-ch-ua-platform': '"macOS"',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-dest': 'empty',
+        'referer': 'https://www.heapdump.cn/',
+        'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8',
+        'cookie': 'UM_distinctid=17af291f8c49ad-098284a7075cca-35637203-13c680-17af291f8c5b77; _ga=GA1.2.1179930627.1635835930; Hm_lvt_3408298b960e49eb328fcacc70d124c5=1638237736,1639967025,1640093361,1640223854; CNZZDATA1278052687=740206226-1627565290-https%253A%252F%252Fheapdump.cn%252F%7C1640569458; _gid=GA1.2.344283848.1640569459; serviceTicket=760214901c414424b6e97df00d1f393e; JSESSIONID=4C7C163F68D083FEFC3485BC53E208A6; Hm_lpvt_3408298b960e49eb328fcacc70d124c5=1640575072'
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+    print(response.text)
+    return response.json().get('data')
+
 def login():
     login_url = "https://www.heapdump.cn/api/login/authentication/v1/login"
     payload = "{\"account\":\"%s\",\"passwd\":\"%s\"}" % (ACCOUNT, PASSWD)
@@ -78,8 +128,15 @@ print("begin to handle heapdump task. sleep_time: %ss" % (sleep_time))
 send_notify(
     "Heapdump", "begin to handle heapdump task. sleep_time: %ss" % (sleep_time))
 time.sleep(sleep_time)
+visitHome()
 login()
-time.sleep(randrange(1, SLEEP_TIME_MIN*10))
-addSignin()
-print("finish the heapdump task.")
-send_notify("Heapdump", "finish the heapdump task.")
+visitHome()
+signin = isSignin()
+if(signin):
+    print("already signin")
+    send_notify("Heapdump", "already signin.")
+else:
+    time.sleep(randrange(1, SLEEP_TIME_MIN*10))
+    addSignin()
+    print("finish the heapdump task.")
+    send_notify("Heapdump", "finish the heapdump task.")

4fad0f214a095a97c5e27135f6dfc99b15c0cc00
ret0x7c00 <45315543+ret0x7c00@users.noreply.github.com>
2022-06-03 19:15:52 +0800

    Update heapdump-robot.py

--- heapdump-robot.py
+++ heapdump-robot.py
@@ -11,80 +11,75 @@ from requests.sessions import session
 ACCOUNT = os.environ.get('HEAPDUMP_ACCOUNT')
 PASSWD = os.environ.get('HEAPDUMP_PASSWD')
 SLEEP_TIME_MIN = os.environ.get('HEAPDUMP_SLEEP_TIME_MIN') or 1
-SLEEP_TIME_MAX = os.environ.get('HEAPDUMP_SLEEP_TIME_MAX') or 6
+SLEEP_TIME_MAX = os.environ.get('HEAPDUMP_SLEEP_TIME_MAX') or 60
 
-NOTIFY_GROUP = 'Heapdump'
+NOTIFY_GROUP = 'Heapdump2'
 
 session = requests.Session()
 
 
 def visitHome():
-    url = "https://www.heapdump.cn/"
+    url = "https://heapdump.cn/"
 
     payload = {}
     headers = {
-        'authority': 'www.heapdump.cn',
-        'pragma': 'no-cache',
-        'cache-control': 'no-cache',
-        'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="96", "Google Chrome";v="96"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"macOS"',
-        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'sec-fetch-site': 'same-origin',
-        'sec-fetch-mode': 'navigate',
-        'sec-fetch-user': '?1',
-        'sec-fetch-dest': 'document',
-        'referer': 'https://www.heapdump.cn/',
-        'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+        'Accept-Language': 'zh-CN,en-US;q=0.7,en;q=0.3',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'DNT': '1',
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1',
+        'Sec-Fetch-Dest': 'document',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-Site': 'none',
+        'Sec-Fetch-User': '?1',
+        'TE': 'trailers'
     }
     response = session.request("GET", url, headers=headers, data=payload)
     print(response.status_code)
 
 
 def isSignin():
-    url = "https://www.heapdump.cn/api/community/signin/isSignin"
+    url = "https://heapdump.cn/api/community/signin/isSignin"
 
     payload = {}
     headers = {
-        'authority': 'www.heapdump.cn',
-        'pragma': 'no-cache',
-        'cache-control': 'no-cache',
-        'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="96", "Google Chrome";v="96"',
-        'accept': 'application/json, text/plain, */*',
-        'dnt': '1',
-        'sec-ch-ua-mobile': '?0',
-        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',
-        'sec-ch-ua-platform': '"macOS"',
-        'sec-fetch-site': 'same-origin',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-dest': 'empty',
-        'referer': 'https://www.heapdump.cn/',
-        'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8',
-        'cookie': 'UM_distinctid=17af291f8c49ad-098284a7075cca-35637203-13c680-17af291f8c5b77; _ga=GA1.2.1179930627.1635835930; Hm_lvt_3408298b960e49eb328fcacc70d124c5=1638237736,1639967025,1640093361,1640223854; CNZZDATA1278052687=740206226-1627565290-https%253A%252F%252Fheapdump.cn%252F%7C1640569458; _gid=GA1.2.344283848.1640569459; serviceTicket=760214901c414424b6e97df00d1f393e; JSESSIONID=4C7C163F68D083FEFC3485BC53E208A6; Hm_lpvt_3408298b960e49eb328fcacc70d124c5=1640575072'
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',
+        'Accept': 'application/json, text/plain, */*',
+        'Accept-Language': 'zh-CN,en-US;q=0.7,en;q=0.3',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'DNT': '1',
+        'Connection': 'keep-alive',
+        'Referer': 'https://heapdump.cn/',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-origin',
+        'TE': 'trailers'
     }
 
     response = session.request("GET", url, headers=headers, data=payload)
     print(response.text)
     return response.json().get('data')
 
+
 def login():
-    login_url = "https://www.heapdump.cn/api/login/authentication/v1/login"
+    login_url = "https://heapdump.cn/api/login/authentication/v1/login"
     payload = "{\"account\":\"%s\",\"passwd\":\"%s\"}" % (ACCOUNT, PASSWD)
     headers = {
-        'authority': 'www.heapdump.cn',
-        'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="98", "Google Chrome";v="98"',
-        'accept': 'application/json, text/plain, */*',
-        'content-type': 'application/json;charset=UTF-8',
-        'sec-ch-ua-mobile': '?0',
-        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4741.0 Safari/537.36',
-        'sec-ch-ua-platform': '"macOS"',
-        'origin': 'https://www.heapdump.cn',
-        'sec-fetch-site': 'same-origin',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-dest': 'empty',
-        'referer': 'https://www.heapdump.cn/',
-        'accept-language': 'en-US,en;q=0.9',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',
+        'Accept': 'application/json, text/plain, */*',
+        'Accept-Language': 'zh-CN,en-US;q=0.7,en;q=0.3',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Content-Type': 'application/json;charset=utf-8',
+        'Origin': 'https://heapdump.cn',
+        'DNT': '1',
+        'Connection': 'keep-alive',
+        'Referer': 'https://heapdump.cn/',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-origin',
+        'TE': 'trailers'
     }
     response = session.request(
         "POST", login_url, headers=headers, data=payload)
@@ -92,21 +87,21 @@ def login():
 
 
 def addSignin():
-    signin_url = "https://www.heapdump.cn/api/community/signin/addSignin"
+    signin_url = "https://heapdump.cn/api/community/signin/addSignin"
     payload = {}
     headers = {
-        'authority': 'www.heapdump.cn',
+        'authority': 'heapdump.cn',
         'content-length': '0',
         'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="98", "Google Chrome";v="98"',
         'accept': 'application/json, text/plain, */*',
         'sec-ch-ua-mobile': '?0',
-        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4741.0 Safari/537.36',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',
         'sec-ch-ua-platform': '"macOS"',
-        'origin': 'https://www.heapdump.cn',
+        'origin': 'https://heapdump.cn',
         'sec-fetch-site': 'same-origin',
         'sec-fetch-mode': 'cors',
         'sec-fetch-dest': 'empty',
-        'referer': 'https://www.heapdump.cn/',
+        'referer': 'https://heapdump.cn/',
         'accept-language': 'en-US,en;q=0.9',
     }
     response = session.request(
